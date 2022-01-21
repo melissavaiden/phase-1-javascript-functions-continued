34c84340e8b8c029ea456c125ce4072f51572bcc
Melissa Vaiden <melissavaiden@gmail.com>
2022-01-21 11:38:15 -0500

    Completed Assigment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function saturdayFun (activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+const mondayWork = function(work = 'go to the office') {
+    return `This Monday, I will ${work}.`;
+}
+function wrapAdjective(exclaim = `*`) {
+      return function (adj = `special`) {
+        return ( `You are ${exclaim}${adj}${exclaim}!`);
+      };
+  }
